05e6d74f2d2b014317d95e00b2d0185d3a6fa05a
Anders Hoff <inconvergent@gmail.com>
2016-08-09 19:36:56 +0200

    working grid of spheres

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const THREE = require('three');
 
-//const HUSL = require('husl');
+const THREE = require('three');
+const HUSL = require('husl');
+var OrbitControls = require('three-orbit-controls')(THREE)
 
 let scene;
 let camera;
 let renderer;
-let geometry;
-let material;
-let mesh;
 
-const origin = new THREE.Vector3(0, 0, 0);
+//const origin = new THREE.Vector3(0, 0, 0);
 
 let lastRender = new Date();
 
@@ -24,20 +22,34 @@ let lastRender = new Date();
 //  mouseDown = 0;
 //}
 
-//const birth = () => {
-//  //let loc = origin.clone()
-//  //if(scene.children.length>0) {
-//  //  let parent =scene.children[Math.random()*(scene.children.length-1)|0];
-//  //  loc.copy(parent.position);
-//  //}
-//  //let color  =  HUSL.toHex(Math.random()*360|0, 65,60);
-//  //let material = new THREE.MeshBasicMaterial( { color, transparent: true, opacity: 0.8 } )
-//  //geometry = new THREE.SphereGeometry( .1, 7, 7 )
-//  //mesh = new THREE.Mesh( geometry, material )
-//  //mesh.position.copy(loc)
-//  //mesh.velocity = new THREE.Vector3((Math.random()-0.5),(Math.random()-0.5),(Math.random()-0.5))
-//  //scene.add( mesh )
-//};
+const grid = () => {
+  const num = 50;
+  const width = 30;
+  const rad = 0.1;
+  const polyCount = 3;
+
+  const color = HUSL.toHex(180, 65, 60);
+  const material = new THREE.MeshBasicMaterial({
+    color,
+    transparent: true,
+    opacity: 0.8
+  });
+  const geom = new THREE.SphereGeometry(rad, polyCount, polyCount);
+  const sphere = new THREE.Mesh(geom, material);
+
+  for (let j = 0; j < num; j++){
+    for (let i = 0; i < num; i++){
+      const s = sphere.clone();
+      s.position.set(
+        -0.5 * width + (i / num * width),
+        -0.5 * width + (j / num * width),
+         0
+      );
+      //console.log(s);
+      scene.add(s);
+    }
+  }
+};
 
 const init = () => {
   scene = new THREE.Scene();
@@ -45,42 +57,20 @@ const init = () => {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
   camera.position.z = 22;
 
+  // eslint-disable-next-line
+  const controls = new OrbitControls(camera);
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(0xfaf7f0, 1);
+
+  grid();
   document.body.appendChild(renderer.domElement);
 };
 
 
 const animate = () => {
   requestAnimationFrame(animate);
-  //scene.children.forEach(me => {
-  //if(me.type!=="Mesh") return
-
-  //me.geometry.normalize()
-  //let scale =  1 + (1 * Math.cos(Date.now()/10000))
-  //me.geometry.scale(scale,scale,scale);
-
-  //let move = origin.clone()
-  //scene.children.forEach(friend => {
-  //  if(friend === me || friend.type !== "Mesh") return
-  //  let distance = friend.position.clone().sub(me.position)
-  //  let delta = distance.length()
-  //  if(delta < 3) //avoid close neighbors
-  //    move.sub(distance.setLength(Math.pow(delta, -1) * 10))
-  //  else if(delta > 150) //attract to further neighbors
-  //    move.add(distance.max(25))
-  //  else
-  //    mouseDown ? //rotate center
-  //    move.add(origin.clone().crossVectors(me.velocity,friend.velocity).divideScalar(10)) :
-  //    move.add(me.position.clone().cross(distance))
-  //    // move.sub(me.position.clone().cross(distance)) :
-  //
-  //})
-  //me.velocity.add(move.normalize().divideScalar(20))
-  //me.velocity.sub(me.position.clone().divideScalar(cursor.y * 2))
-  //me.position.add(me.velocity.normalize().divideScalar( 1 + (cursor.x / 100)))
-  //})
 
   const c = Date.now();
   const tickTime = c - lastRender;
